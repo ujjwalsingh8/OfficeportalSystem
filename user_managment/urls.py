a4bc615c0e6edab4ebcd43ba195e6a1b7cd22d2f
ujjwalrai <singhujjwal8228@gmail.com>
2024-11-25 12:16:11 +0530

    Passwoed reset

--- user_managment/urls.py
+++ user_managment/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='reset_pass/password_reset_form.html', success_url ='/password-reset/done/'), name='password_reset'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset_pass/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='reset_pass/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_pass/password_reset_complete.html'), name='password_reset_complete'),
     path('signup/', views.signUp, name='signup'),
     path('admin-approval/', views.admin_approval, name='admin_approval'),
     path('login/', views.sign_in, name='login'),
